68aa3d04d1e3fb987a04598d964d38c26816aa92
dengqinghua <dengqinghua@rd.tuan800.com>
2014-12-06 17:15:57 +0800

    add readme

--- 2/intervals/practice.rb
+++ 2/intervals/practice.rb
@@ -1,5 +1,6 @@
 require_relative 'interval_guesser'
 
+puts 'Begin to guess interval'
 12.times do |time|
   puts "== Guess #{time + 1} =="
   guesser = IntervalGuesser.new(has_deviation: ARGV.first)
